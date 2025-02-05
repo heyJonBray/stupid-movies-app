0dc3888537a5d5930616d833b82f7f34d886c5da
Jon Bray <jon@heyjonbray.com>
2025-02-05 01:13:10 -0500

    chore: prep for global state mgmt

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,7 @@ const Navbar = () => {
     <>
       <Overlays isOpen={isOpen} />
       <div className="bg-gradient-to-r from-[rgba(88,29,252,0.6)] to-[rgba(79,43,186,0.6)] w-full h-full flex items-center justify-left">
-        <p
-          className="text-white font-bold ml-5"
-          onClick={() => setIsOpen(true)}
-        >
+        <p className="text-white font-bold ml-5" onClick={() => {}}>
           SMSP
         </p>
       </div>
