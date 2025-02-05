9ead52b92a6b19a6c9baa1310823443506992d08
Jon Bray <jon@heyjonbray.com>
2025-02-05 01:26:40 -0500

    fix: disabled Overlays component in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const setUi = useSetAtom(uiAtom);
   return (
     <>
-      <Overlays />
+      {/* <Overlays /> */}
       <div className="bg-gradient-to-r from-[rgba(88,29,252,0.6)] to-[rgba(79,43,186,0.6)] w-full h-full flex items-center justify-left">
         <p
           className="text-white font-bold ml-5"
