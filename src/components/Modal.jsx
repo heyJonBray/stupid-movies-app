d2de3dfbe3b019ac515ab067a8eeadad30ac9da5
Jon Bray <jon@heyjonbray.com>
2025-02-05 01:25:20 -0500

    fix: working on broken app
    jotai is breaking the app, overlay is not loading, but app is blank;
    overlay component likely broken;

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,22 @@
+import { useSetAtom } from 'jotai';
+import { uiAtom } from '../state';
+
 const Modal = () => {
+  const setUi = useSetAtom(uiAtom);
   return (
     <div className="modal">
       <div className="modal-content">
-        <button className="close-btn">&times;</button>
+        <button
+          className="close-btn"
+          onClick={() =>
+            setUi((prev) => ({
+              ...prev,
+              modal: null,
+            }))
+          }
+        >
+          &times;
+        </button>
         <h2>My Modal</h2>
         <p>ipsum lorem ipsum lorem</p>
       </div>
